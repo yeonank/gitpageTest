fef14c453cdbe640462f4f18f4abd9e1d549e851
yeonank <yeonank99@gmail.com>
2023-07-26 13:52:23 +0900

    추가

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
-    return <Router>
+    return <Router basename={process.env.PUBLIC_URL}>
         <Routes> 
-            <Route path="/movie/:id" element = {<Detail/>}></Route>
-            <Route path="/" element={<Home/>}>
+            <Route path={`/movie/:id`} element = {<Detail/>}></Route>
+            <Route path={`/`}  element={<Home/>}>
             </Route>
         </Routes>
     </Router>;
